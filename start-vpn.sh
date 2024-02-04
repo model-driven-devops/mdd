716c47b1e0c1d0c07525d0f956833e912b11afcf
Jason King <jk1ng@yahoo.com>
2024-02-04 15:39:20 +0100

    Updated start-vpn.sh

--- start-vpn.sh
+++ start-vpn.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 read -p "Enter password: " VPN_PASSWORD
-printf "nsouser1\n$VPN_PASSWORD\ny" | /opt/cisco/secureclient/bin/vpn connect devnetsandbox-usw1-reservation.cisco.com:20397
+printf "nsouser1\n$VPN_PASSWORD\ny" | /opt/cisco/secureclient/bin/vpn -s connect devnetsandbox-usw1-reservation.cisco.com:20397
